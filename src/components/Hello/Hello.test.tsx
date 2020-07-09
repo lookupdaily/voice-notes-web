ec5f9aa1520ff43061f05459b8f92bf0fac0c63b
Liz Daly <liz7daly@gmail.com>
2020-07-09 19:20:14 +0100

    Create `Hello` component

---
+++ src/components/Hello/Hello.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Hello } from './Hello';
+
+describe('Hello', () => {
+  test('renders greeting', () => {
+    const wrapper = shallow(<Hello appName="Taped It"/>)
+    expect(wrapper.find('h1').text()).toEqual('Say Hello to Taped It!')
+  })
+})
