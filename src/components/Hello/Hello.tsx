dc4f009b6a5faf234f57f0e66eb4e9b55737d0c9
Liz Daly <liz7daly@gmail.com>
2020-07-09 19:30:22 +0100

    Remove export on HelloProps interface

--- src/components/Hello/Hello.tsx
+++ src/components/Hello/Hello.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 
-export interface HelloProps { appName: string }
+interface HelloProps { appName: string }
 
 export const Hello = (props: HelloProps) => <h1>Say Hello to {props.appName}!</h1>
